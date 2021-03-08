a2a4ead1e3d99f7ce84bd5352b84d1f66eacd796
Garfill <junhui7guan@163.com>
2021-03-08 09:58:35 +0800

    alias

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Switch, BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { routes } from 'route';
+
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      {routes.map(route => {
+        return (
+          <Route key={route.path} {...route}></Route>
+        )
+      })}
+      <Redirect to='/404'></Redirect>
+    </Switch>
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
