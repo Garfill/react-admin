2fb1d34b16d42a0b6ff90e65f974dd1695b0f0e3
Garfill <junhui7guan@163.com>
2021-06-01 18:30:07 +0800

    fixed: 修复input失焦问题

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,38 +1,65 @@
 import React, { Component, createRef } from 'react'
 import CountTo from 'components/CountTo'
 import styled from 'styled-components'
-import { Button } from 'antd';
+import { Button, Input } from 'antd';
 export class Home extends Component {
   constructor(props) {
     super(props);
     this.countToRef = createRef();
+
+    this.state = {
+      startVal: 0,
+      endVal: 100,
+    }
   }
   render() {
-
-    const Container = styled.div`
-      padding: 16px;
-      border-radius: 5px;
-      background: #fff;
-    `
     const CountToContainer = styled.div`
+      margin-bottom: 16px;
       font-size: 24px;
       color: #444;
     `
+    const ButtonContainer = styled.div`
+      margin-top: 16px;
+    `
 
     return (
-      <Container>
+      <div className="box-container">
+        <h2>计数器（Counter）</h2>
         <CountToContainer>
-          <CountTo start={0} end={100} loop={true} ref={this.countToRef}></CountTo>
+          <CountTo start={this.state.startVal} end={this.state.endVal} ref={this.countToRef}></CountTo>
         </CountToContainer>
-        <Button style={{marginRight: '8px'}} onClick={() => this.clickBtn('start')} type="primary">start</Button>
-        <Button style={{marginRight: '8px'}} onClick={() => this.clickBtn('pause')}>pause</Button>
-        <Button style={{marginRight: '8px'}} onClick={() => this.clickBtn('resume')} type="primary">resume</Button>
-        <Button style={{marginRight: '8px'}} onClick={() => this.clickBtn('reset')}>reset</Button>
-      </Container>
+        <div>
+          <span>开始值</span>
+          <Input value={this.state.startVal} onChange={this.handleStartChange}></Input>
+        </div>
+        <div>
+          <span>结束值</span>
+          <Input value={this.state.endVal} onChange={this.handleEndChange}></Input>
+        </div>
+        <ButtonContainer>
+          <Button style={{marginRight: '8px'}} onClick={() => this.clickBtn('start')} type="primary">start</Button>
+          <Button style={{marginRight: '8px'}} onClick={() => this.clickBtn('pause')}>pause</Button>
+          <Button style={{marginRight: '8px'}} onClick={() => this.clickBtn('resume')} type="primary">resume</Button>
+          <Button style={{marginRight: '8px'}} onClick={() => this.clickBtn('reset')}>reset</Button>
+        </ButtonContainer>
+      </div>
     )
   }
+
   clickBtn = (func) => {
     this.countToRef.current[func]();
+  }
+  
+  handleStartChange = (e) => {
+    this.setState({
+      startVal: Number(e.target.value)
+    })
+  }
+
+  handleEndChange = (e) => {
+    this.setState({
+      endVal: Number(e.target.value)
+    })
   }
 }
 
